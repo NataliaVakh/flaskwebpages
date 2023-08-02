09e3185b1bc0ee3846a2bdc9b343a11ba6e45eda
Natalia Vakh <vnatv1982@mail.ru>
2023-08-02 15:58:28 +0000

    Adding tests

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
 from flask_sqlalchemy import SQLAlchemy
-import git
+# import git
 
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)
